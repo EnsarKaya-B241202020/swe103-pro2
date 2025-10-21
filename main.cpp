77b7f6b20e942a2d7d734d13ffb09b7365153d2f
Ensar Kaya <ensar.kaya2@ogr.sakarya.edu.tr>
2025-10-21 10:05:04 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    cout << "Hello, SWE 103!" << endl;
+    int i=5 , j=10;
+    cout << "The sum of " << i << " and " << j << " is " << i + j << endl;
+
+    cout << "This program is compiled using C++14 standard." << endl;
+    
+    system("pause");
+    return 0;
+    
+}
